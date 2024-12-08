40840e41d6a35beaf4cba939d5eceed18087461c
Tirth1107 <amitkumarjoshi71@gmail.com>
2024-12-08 19:03:28 +0530

    Add files via upload

---
+++ mainfile.js
@@ -0,0 +1,3 @@
+// Add any JavaScript functionality here (if needed)
+// For example, for interactive elements, modals, or form validations
+console.log("mainfile.js loaded");  // This will log to the browser's console to confirm the JS file is working
